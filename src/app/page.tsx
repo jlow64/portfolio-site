4995fd3269eb556d5061ae3189477b904f87ff98
jlow64 <justin64low@gmail.com>
2023-10-18 19:27:12 +1300

    Screen animation added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,89 @@
 'use client';
 
-import { useState } from 'react';
+import { useLayoutEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import styles from './page.module.css';
 import { plusJakarta, oswald } from '@/fonts';
+import { animations } from '@/constants';
+import gsap from 'gsap';
 
 export default function Home() {
+  const app = useRef(null);
+  const powerCharge = useRef(null);
+  const frenzyCharge = useRef(null);
+  const enduranceCharge = useRef(null);
+
   const [selectedPage, setSelectedPage] = useState<
     'home' | 'portfolio' | 'about' | 'resume'
   >('home');
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap
+        .timeline()
+        .fromTo(
+          enduranceCharge.current,
+          animations.enduranceCharge.a1,
+          animations.enduranceCharge.a2,
+        )
+        .to(enduranceCharge.current, animations.enduranceCharge.a3)
+        .to(enduranceCharge.current, animations.enduranceCharge.a4)
+        .repeat(-1);
+      gsap
+        .timeline()
+        .fromTo(
+          powerCharge.current,
+          animations.powerCharge.a1,
+          animations.powerCharge.a2,
+        )
+        .to(powerCharge.current, animations.powerCharge.a3)
+        .to(powerCharge.current, animations.powerCharge.a4)
+        .repeat(-1);
+      gsap
+        .timeline()
+        .fromTo(
+          frenzyCharge.current,
+          {
+            x: -100,
+            y: 300,
+            scale: 0.8,
+            opacity: 1,
+            webkitFilter: 'brightness(1.2)',
+            filter: 'brightness(1.2)',
+          },
+          {
+            x: 150,
+            y: 200,
+            scale: 0.65,
+            duration: 1,
+            webkitFilter: 'brightness(0.95)',
+            filter: 'brightness(0.95)',
+            ease: 'power2.out',
+          },
+        )
+        .to(frenzyCharge.current, {
+          x: -100,
+          y: 100,
+          scale: 0.5,
+          duration: 1,
+          webkitFilter: 'brightness(0.8)',
+          filter: 'brightness(0.8)',
+          ease: 'power2.out',
+        })
+        .to(frenzyCharge.current, {
+          x: -100,
+          y: 300,
+          scale: 0.8,
+          duration: 1,
+          webkitFilter: 'brightness(1.2)',
+          filter: 'brightness(1.2)',
+          ease: 'power2.out',
+        })
+        .repeat(-1);
+    }, app);
+
+    return () => ctx.revert();
+  });
 
   return (
     <main
@@ -50,19 +125,27 @@ export default function Home() {
       </div>
       <div className={styles.pageContainer}>
         <Image
+          // onMouseEnter={onMouseEnter}
+          // onMouseLeave={onMouseLeave}
+          className={styles.charges}
           src="/images/endurance-charge.svg"
+          ref={enduranceCharge}
           width={100}
           height={100}
           alt="endurance charge"
         />
         <Image
+          className={styles.charges}
           src="/images/power-charge.svg"
+          ref={powerCharge}
           width={100}
           height={100}
           alt="power charge"
         />
         <Image
+          className={styles.charges}
           src="/images/frenzy-charge.svg"
+          ref={frenzyCharge}
           width={100}
           height={100}
           alt="frenzy charge"
