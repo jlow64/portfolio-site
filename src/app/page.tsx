4eee4dff23726895ab3add8d101e075b6c28e8db
jlow64 <justin64low@gmail.com>
2023-11-29 18:38:44 +1300

    Masking attempt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 'use client';
 
+import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Perf } from 'r3f-perf';
-import { Loader } from '@react-three/drei';
+import { Loader, Environment, ContactShadows } from '@react-three/drei';
 import Experience from '@/components/Experience';
 import styles from './page.module.css';
 import { plusJakarta, oswald } from '@/styles/fonts';
 
-export default function Home() {
+export default function App() {
   return (
-    <main
-      className={`${styles.main} ${plusJakarta.variable} ${oswald.variable}`}
+    <div
+      className={`${styles.domContent} ${plusJakarta.variable} ${oswald.variable}`}
     >
       <Canvas
         flat
@@ -23,9 +24,20 @@ export default function Home() {
         dpr={[1, 2]}
       >
         <Perf position="top-left" />
-        <Experience />
+        <Suspense fallback={null}>
+          <Experience />
+          <Environment preset="city" />
+          <color args={['#241a1a']} attach="background" />
+          <ContactShadows
+            position-y={-1.6}
+            opacity={0.4}
+            scale={5}
+            blur={2.4}
+          />
+        </Suspense>
       </Canvas>
       <Loader />
-    </main>
+    </div>
+    // </div>
   );
 }
--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,13 +1,5 @@
-import {
-  Text,
-  Html,
-  ContactShadows,
-  Environment,
-  Float,
-  PresentationControls,
-  useGLTF,
-} from '@react-three/drei';
-import { useRef, Suspense } from 'react';
+import { Text, Float, PresentationControls, useGLTF } from '@react-three/drei';
+import { useRef } from 'react';
 import { Mesh } from 'three';
 import { useThree, useFrame } from '@react-three/fiber';
 import Macbook from '@/components/Macbook';
@@ -15,7 +7,6 @@ import Iphone from '@/components/Iphone';
 
 export default function Experience() {
   const catRef = useRef<Mesh>(null!);
-
   const cat = useGLTF('./models/Dingus the cat.glb');
 
   const { viewport } = useThree();
@@ -28,71 +19,64 @@ export default function Experience() {
   );
 
   return (
-    <>
-      <Environment preset="city" />
-      <color args={['#241a1a']} attach="background" />
-      <PresentationControls
-        global
-        rotation={[0.13, 0.1, 0]}
-        polar={isDesktop ? [-0.4, 0.2] : [-0.2, 0]}
-        azimuth={[-1, 0.75]}
-        config={{ mass: 2, tension: 400 }}
-        snap={{ mass: 4, tension: 400 }}
-      >
-        <Suspense fallback={null}>
-          <Float rotationIntensity={0.4}>
-            <rectAreaLight
-              width={2.5}
-              height={1.65}
-              intensity={65}
-              color={'#0f5e9e'}
-              rotation={[-0.1, Math.PI, 0]}
-              position={[0, 0.55, -1.15]}
-            />
-            {/* Macbook */}
-            <Macbook
-              visible={isDesktop}
-              position-y={-1.2}
-              rotation-x={0.13}
-              scale={viewport.width / 8}
-            />
-            {/* iphone */}
-            <Iphone
-              visible={!isDesktop}
-              position-y={-1.2}
-              rotation-x={-0.3}
-              rotation-y={-0.2}
-            />
+    <PresentationControls
+      global
+      rotation={[0.13, 0.1, 0]}
+      polar={isDesktop ? [-0.4, 0.2] : [-0.2, 0]}
+      azimuth={[-1, 0.75]}
+      config={{ mass: 2, tension: 400 }}
+      snap={{ mass: 4, tension: 400 }}
+    >
+      <Float rotationIntensity={0.4}>
+        <rectAreaLight
+          width={2.5}
+          height={1.65}
+          intensity={65}
+          color={'#0f5e9e'}
+          rotation={[-0.1, Math.PI, 0]}
+          position={[0, 0.55, -1.15]}
+        />
+        {/* Macbook */}
+        <Macbook
+          visible={isDesktop}
+          position-y={-1.2}
+          rotation-x={0.13}
+          scale={viewport.width / 8}
+        />
+        {/* iphone */}
+        <Iphone
+          visible={!isDesktop}
+          position-y={-1.2}
+          rotation-x={-0.3}
+          rotation-y={-0.2}
+        />
 
-            {/* Cat */}
-            <primitive
-              visible={isDesktop}
-              ref={catRef}
-              object={cat.scene}
-              position-x={viewport.width / 3.8}
-              position-y={-0.8}
-              rotation-y={-0.2}
-              scale={viewport.width / 20}
-            />
+        {/* Cat */}
+        <primitive
+          visible={isDesktop}
+          ref={catRef}
+          object={cat.scene}
+          position-x={viewport.width / 3.8}
+          position-y={-0.8}
+          rotation-y={-0.2}
+          scale={viewport.width / 20}
+        />
 
-            <Text
-              font="./fonts/PlusJakartaSans.ttf"
-              fontSize={viewport.width / 15}
-              position={
-                isDesktop
-                  ? [viewport.width / 5, viewport.width / 9, 0.75]
-                  : [0, -1.3, 1]
-              }
-              rotation={isDesktop ? [0, -1, 0] : [-0.3, -0.4, -0.2]}
-              maxWidth={isDesktop ? 2 : 1}
-              textAlign="center"
-            >
-              JUSTIN LOW
-            </Text>
-          </Float>
-        </Suspense>
-      </PresentationControls>
-      <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4} />
-    </>
+        <Text
+          font="./fonts/PlusJakartaSans.ttf"
+          fontSize={viewport.width / 15}
+          position={
+            isDesktop
+              ? [viewport.width / 5, viewport.width / 9, 0.75]
+              : [0.5, -1.3, 0.9]
+          }
+          rotation={isDesktop ? [0, -1, 0] : [-0.3, -0.4, -0.2]}
+          maxWidth={isDesktop ? 2 : 1}
+          textAlign="center"
+        >
+          JUSTIN LOW
+        </Text>
+      </Float>
+    </PresentationControls>
   );
 }
